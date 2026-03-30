342de22abdce7234284e0635a2d44b9053fbb667
Deep Gupta <deep2004gupta@gmail.com>
2026-03-30 19:52:25 +0530

    155

---
+++ Stack/155-Min-Stack.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class MinStack {
+    Stack<Integer> st;
+    Stack<Integer> minSt;
+
+    public MinStack() {
+        st = new Stack<>();
+        minSt = new Stack<>();
+    }
+    
+    public void push(int val) {
+        st.push(val);
+        
+        if (minSt.isEmpty() || val <= minSt.peek()) {
+            minSt.push(val);
+        }
+    }
+    
+    public void pop() {
+        int removed = st.pop();
+        
+        if (removed == minSt.peek()) {
+            minSt.pop();
+        }
+    }
+    
+    public int top() {
+        return st.peek();
+    }
+    
+    public int getMin() {
+        return minSt.peek();
+    }
+}
