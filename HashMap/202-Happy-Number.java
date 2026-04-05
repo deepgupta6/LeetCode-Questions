483f08272d540e0987f50f0fdf0bdd6348b872aa
Deep Gupta <deep2004gupta@gmail.com>
2026-04-05 12:01:05 +0530

    202

---
+++ HashMap/202-Happy-Number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> set = new HashSet<>();
+
+        while (n != 1) {
+            int sum = 0;
+
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+
+            if (set.contains(sum)) return false;
+
+            set.add(sum);
+            n = sum;
+        }
+
+        return true;
+    }
+}
